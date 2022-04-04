dbbe55e12532cdfa2d6eb5bd3d7bea5a0988deb6
Sean O'Hara <seano660@outlook.com>
2022-04-03 21:39:25 -0400

    test

--- components/preprocess_data/run.py
+++ components/preprocess_data/run.py
@@ -24,7 +24,8 @@ def go(args):
     data = data[data["Thread Entry Type"] != "share"]
 
     data["Full Text"] = (
-        data["Full Text"].fillna(" ")
+        data["Full Text"].astype(str)
+        .fillna(" ")
         .str
         .replace("http\S*\s?", "") # remove links
         .replace("\s+", " ") # replace any escape character with space
