9f1ac66633751e56430ac1ed45fbd0f2db2f6876
Sean O'Hara <seano660@outlook.com>
2022-04-04 22:58:31 -0400

    fix

--- components/create_corpus/run.py
+++ components/create_corpus/run.py
@@ -43,7 +43,8 @@ def go(args):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("mode", type = str, help = "Run mode (local or remote)")
-    parser.add_argument("input_path", type = str, help = "Path to input data")     
+    parser.add_argument("input_path", type = str, help = "Path to input data")
+    parser.add_argument("train_size", type = float, help = "Portion of data to set aside for training")
     parser.add_argument("random_state", type = int, default = None, help = "Seed for setting the random state")
     parser.add_argument("vocab_size", type = int, default = None, help = "Max. # of words to include in corpus") 
     parser.add_argument("no_below", type = int, default = None, help = "Min. # of occurrences to include in corpus")
